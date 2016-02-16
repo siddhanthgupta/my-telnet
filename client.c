8fdcf120962f992ee39f63b03526c08f5e47d01a
Siddhanth Gupta <siddhanthgupta@gmail.com>
2016-02-16 09:57:29 +0530

    Improve negotiation parameters

--- client.c
+++ client.c
@@ -19,9 +19,26 @@
 #define BUFLEN 200
 #define ESCAPE 27
 
+/**
+ * Negotiation for the telnet terminal. Accepts negotiation IAC from server __buf
+ * of __len from __sock. Sent before starting the logon process.
+ *
+ * It is not required that you implementing telnet negotiation in any clients
+ * and servers you may create, but you need to be aware of it in order to avoid
+ * problems that its presence on the other end of your connection may otherwise
+ * cause.
+ *
+ * See telnet protocol specification for more details:
+ * https://tools.ietf.org/html/rfc854
+ *
+ * @param sock  The socket we use to connect to the telnet server
+ * @param buf   The buffer that contains the IAC (Is A Command) sent from the
+ *              server
+ * @param len   The len of the message
+ */
 void negotiate(int sock, unsigned char *buf, int len) {
     int i;
-    const char* option_code[50];
+    const char* option_code[350];
     option_code[00] = "TRANSMIT-BINARY";
     option_code[01] = "ECHO";
     option_code[03] = "SUPPRESS-GO-AHEAD";
@@ -59,25 +76,38 @@ void negotiate(int sock, unsigned char *buf, int len) {
     option_code[42] = "CHARSET";
     option_code[44] = "COM-PORT-OPTION";
     option_code[47] = "KERMIT";
+    option_code[250] = "SB";
+    option_code[240] = "SE";
+    option_code[251] = "WILL";
+    option_code[252] = "WONT";
+    option_code[253] = "DO";
+    option_code[254] = "DONT";
+    option_code[255] = "IAC";
     if (buf[1] == DO && buf[2] == CMD_WINDOW_SIZE) {
         unsigned char tmp1[10] = { IAC, WILL, CMD_WINDOW_SIZE };
         if (send(sock, tmp1, 3, 0) < 0)
             exit(1);
 
         unsigned char tmp2[10] = { IAC, SB, CMD_WINDOW_SIZE, 0, 80, 0, 24, IAC,
-                SE };
+        SE };
         if (send(sock, tmp2, 9, 0) < 0)
             exit(1);
         return;
     }
 
-    for (i = 0; i < len; i++) {
+    /*
+     * To avoid any problems, we reply to every DO with a WONT and every
+     * WILL with a DO. Both of these are not required, but are included to prevent
+     * any problems with most telnet servers.
+     *
+     * Echoing back the same characters to the server works on Linux machines,
+     * but may not work on other terminals.
+     *
+     */
+    for (i = 1; i < len; i++) {
         if (buf[i] == DO) {
-            printf("Server sent DO %s\n\r", option_code[buf[i + 1]]);
             buf[i] = WONT;
         } else if (buf[i] == WILL) {
-
-            printf("Server sent WILL %s\n\r", option_code[buf[i + 1]]);
             buf[i] = DO;
         }
     }
@@ -94,7 +124,8 @@ static void terminal_set(void) {
 
     static struct termios tlocal;
     memcpy(&tlocal, &tin, sizeof(tin));
-    // The file descriptor which has to be turned to raw mode is the standard input of the parent process
+    // The file descriptor which has to be turned to raw mode is the standard
+    // input of the parent process
     cfmakeraw(&tlocal);
     tcsetattr(STDIN_FILENO, TCSANOW, &tlocal);
 }
@@ -104,9 +135,21 @@ static void terminal_reset(void) {
     tcsetattr(STDIN_FILENO, TCSANOW, &tin);
 }
 
+void connect_to_server(int sock, int port, char* address) {
+    struct sockaddr_in server;
+    server.sin_addr.s_addr = inet_addr(address);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    //Connect to remote server
+    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        perror("connect failed. Error");
+        exit(1);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int sock;
-    struct sockaddr_in server;
     unsigned char buf[BUFLEN + 1];
     int len;
     int i;
@@ -125,17 +168,12 @@ int main(int argc, char *argv[]) {
         perror("Could not create socket. Error");
         return 1;
     }
+    printf("Trying %s...", argv[1]);
 
-    server.sin_addr.s_addr = inet_addr(argv[1]);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
+    connect_to_server(sock, port, argv[1]);
 
-    //Connect to remote server
-    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        perror("connect failed. Error");
-        return 1;
-    }
-    puts("Connected...\n");
+    printf("Connected to %s\n", argv[1]);
+    puts("Escape character is '^]'.");
 
     // set terminal
     terminal_set();
@@ -159,7 +197,7 @@ int main(int argc, char *argv[]) {
             perror("select. Error");
             return 1;
         } else if (nready == 0) {
-            ts.tv_sec = 1; // 1 second
+            ts.tv_sec = 1;
             ts.tv_usec = 0;
         } else if (sock != 0 && FD_ISSET(sock, &fds)) {
             // start by reading a single byte
@@ -193,8 +231,9 @@ int main(int argc, char *argv[]) {
             static char crlf[] = { '\r', '\n' };
             buf[0] = getc(stdin); //fgets(buf, 1, stdin);
             if (buf[0] == '\n') { // with the terminal in raw mode we need to force a LF
-                if (send(sock, crlf, 1, 0) < 0)
+                if (send(sock, crlf, 1, 0) < 0) {
                     return 1;
+                }
             } else if (buf[0] == ESCAPE) {
                 printf("Connection closed by the client end\n\r");
                 return 0;
